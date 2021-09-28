2abc5e2a93026bde1637819a210ee82b49ab22e6
terryfc <terrychie@gmail.com>
2021-09-28 15:30:21 +0800

    category

--- build/resources/main/static/js/joyousProductPage.js
+++ build/resources/main/static/js/joyousProductPage.js
@@ -6,39 +6,20 @@
 const product = new joyousProductController();
 //product _items currently is empty
 
-function loadData()
-{
-//Start adding products into product_items
-/*
-product.addItem(1, "Dream of my Li", "A collection of rose..", "images/Floral Arrangement/Birth01.jpg", "Birthday", "$180");
-product.addItem(2, "Beauty in a Box", "A collection of rose..", "images/Floral Arrangement/Birth02.jpg", "Birthday", "$180");
-product.addItem(3, "Path of Joys", "Walking in the Shade of", "images/Floral Arrangement/Birth03.jpg", "Birthday", "$180");
-product.addItem(4, "Bouquet of Dreams", "A collection of rose..", "images/Floral Arrangement/Birth04.jpg", "Birthday", "$180");
-product.addItem(5, "Pots of Gold & Luck", "A collection of rose..", "images/Floral Arrangement/Birth05.jpg", "Birthday", "$180");
-product.addItem(6, "Sparkling Joys", "A collection of rose..", "images/Floral Arrangement/Birth06.jpg", "Birthday", "$180");
-
-product.addItem(7, "Dream of my Life", "A collection of rose..", "images/Floral Arrangement/Anniver01.jpg", "Anniversary", "$120");
-product.addItem(8, "Dream of my Life", "A collection of rose..", "images/Floral Arrangement/Anniver02.jpg", "Anniversary", "$120");
-product.addItem(9, "Dream of my Life", "A collection of rose..", "images/Floral Arrangement/Anniver03.jpg", "Anniversary", "$120");
-product.addItem(10,"Dream of my Life", "A collection of rose..", "images/Floral Arrangement/Anniver04.jpg", "Anniversary", "$120");
-product.addItem(11,"Dream of my Life", "A collection of rose..", "images/Floral Arrangement/Anniver05.jpg", "Anniversary", "$120");
-product.addItem(12,"Dream of my Life", "A collection of rose..", "images/Floral Arrangement/Anniver06.jpg", "Anniversary", "$120");
-
-product.addItem(13,"Heavenly Hope", "A collection of rose..", "images/Floral Arrangement/Congrat01.jpg", "Congratulatory", "$150");
-product.addItem(14,"Heavenly Hope", "A collection of rose..", "images/Floral Arrangement/Congrat02.jpg", "Congratulatory", "$150");   
-product.addItem(15,"Heavenly Hope", "A collection of rose..", "images/Floral Arrangement/Congrat03.jpg", "Congratulatory", "$150");
-product.addItem(16,"Heavenly Hope", "A collection of rose..", "images/Floral Arrangement/Congrat04.jpg", "Congratulatory", "$150");
-product.addItem(17,"Heavenly Hope", "A collection of rose..", "images/Floral Arrangement/Congrat05.jpg", "Congratulatory", "$150");
-product.addItem(18,"Heavenly Hope", "A collection of rose..", "images/Floral Arrangement/Congrat07.jpg", "Congratulatory", "$150");
-*/
-product.displayItem();
+function loadData() {
+    product.displayItem();
 }
+
 loadData();
 
 // On Select Filter Dropdown
-const select = document.querySelector("#selectCategory");
-select.addEventListener("change", function() { 
-    const selectValue = select.value;
 
+const select = document.querySelector("#selectCategory");
+
+select.addEventListener("change", function () {
+    const selectValue = select.value;
+    // const selectedCategory = 1;
     product.filterItem(selectValue);
+   
+  
 });
